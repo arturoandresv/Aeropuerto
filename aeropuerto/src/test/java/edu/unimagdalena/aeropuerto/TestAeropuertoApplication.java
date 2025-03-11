e62b8d06382e672b014a72c141c494ab8fa3cd4c
arturoandresv <arturovelasquezo0711@gmail.com>
2025-03-11 09:25:58 -0500

    Update TestAeropuertoApplication.java

--- aeropuerto/src/test/java/edu/unimagdalena/aeropuerto/TestAeropuertoApplication.java
+++ aeropuerto/src/test/java/edu/unimagdalena/aeropuerto/TestAeropuertoApplication.java
@@ -2,7 +2,7 @@ package edu.unimagdalena.aeropuerto;
 
 import org.springframework.boot.SpringApplication;
 
-public class TestAeropuertotestApplication {
+public class TestAeropuertoApplication {
 
     public static void main(String[] args) {
         SpringApplication.from(AeropuertoApplication::main).with(TestcontainersConfiguration.class).run(args);
