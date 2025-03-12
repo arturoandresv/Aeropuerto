3cb3064a59ee6c716b59b97d3a1f2c2658b03a3b
arturoandresv <arturovelasquezo0711@gmail.com>
2025-03-11 19:55:12 -0500

    a

--- aeropuerto/src/test/java/edu/unimagdalena/aeropuerto/repositories/AerolineaRepositoryTest.java
+++ aeropuerto/src/test/java/edu/unimagdalena/aeropuerto/repositories/AerolineaRepositoryTest.java
@@ -1,11 +1,19 @@
 package edu.unimagdalena.aeropuerto.repositories;
 
+import edu.unimagdalena.aeropuerto.entities.Aerolinea;
+import edu.unimagdalena.aeropuerto.entities.Vuelo;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.TestcontainersConfiguration;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 @Import(TestcontainersConfiguration.class)
@@ -14,8 +22,39 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AerolineaRepositoryTest {
 
+    @Autowired
+    private AerolineaRepository aerolineaRepository;
+
     @Test
     void findAllByOrderByIdDesc() {
+        Vuelo vuelo1 = Vuelo.builder()
+                .id(1L)
+                .origen("Bogotá")
+                .destino("Medellín")
+                .build();
+
+        Vuelo vuelo2 = Vuelo.builder()
+                .id(2L)
+                .origen("Cali")
+                .destino("Cartagena")
+                .build();
+
+        Set<Vuelo> vuelos = new HashSet<>();
+        vuelos.add(vuelo1);
+        vuelos.add(vuelo2);
+
+        Aerolinea aerolinea = Aerolinea.builder()
+                .nombre("Avianca")
+                .vuelos(vuelos)
+                .build();
+        Aerolinea aerolinea1 = Aerolinea.builder()
+                .nombre("Dorado")
+                .vuelos(vuelos)
+                .build();
+
+        List<Aerolinea> aerolineas = aerolineaRepository.findAllByOrderByIdDesc();
+        Assertions.assertThat(aerolineas.get(0).getNombre()).isEqualTo(aerolinea.getNombre());
+
     }
 
     @Test
--- aeropuerto/src/test/java/edu/unimagdalena/aeropuerto/repositories/PasaporteRepositoryTest.java
+++ aeropuerto/src/test/java/edu/unimagdalena/aeropuerto/repositories/PasaporteRepositoryTest.java
@@ -11,6 +11,10 @@ import org.springframework.context.annotation.Import;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.TestcontainersConfiguration;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 @Import(TestcontainersConfiguration.class)
 @Testcontainers
@@ -23,30 +27,123 @@ class PasaporteRepositoryTest {
 
     @Test
     void findByid() {
+
+        Pasaporte pasaporte = Pasaporte.builder()
+                .numero("12345")
+                .pasajero(Pasajero.builder()
+                        .nombre("Jose")
+                        .NID("6789")
+                        .build())
+                .build();
+
+        pasaporteRepository.save(pasaporte);
+
+        Pasaporte expected = pasaporteRepository.findById(pasaporte.getId()).get();
+        Assertions.assertEquals(expected.getId(), pasaporte.getId());
+
     }
 
     @Test
     void findByNumero() {
+
+        Pasaporte pasaporte = Pasaporte.builder()
+                .numero("12345")
+                .pasajero(Pasajero.builder()
+                        .nombre("Jose")
+                        .NID("6789")
+                        .build())
+                .build();
+
+        pasaporteRepository.save(pasaporte);
+
+        Optional<Pasaporte> expected = pasaporteRepository.findByNumero(pasaporte.getNumero());
+        Assertions.assertTrue(expected.isPresent());
+
     }
 
     @Test
     void findByIdAndNumero() {
+
+        Pasaporte pasaporte = Pasaporte.builder()
+                .numero("12345")
+                .pasajero(Pasajero.builder()
+                        .nombre("Jose")
+                        .NID("6789")
+                        .build())
+                .build();
+
+        pasaporteRepository.save(pasaporte);
+
+        Optional<Pasaporte> expected = pasaporteRepository.findByIdAndNumero(pasaporte.getId(), pasaporte.getNumero());
+        Assertions.assertTrue(expected.isPresent());
     }
 
     @Test
     void findAllByOrderByIdDesc() {
+
+        Pasaporte pasaporte = Pasaporte.builder()
+                .numero("12345")
+                .pasajero(Pasajero.builder()
+                        .nombre("Jose")
+                        .NID("6789")
+                        .build())
+                .build();
+
+        pasaporteRepository.save(pasaporte);
+        List<Pasaporte> expected = pasaporteRepository.findAllByOrderByIdDesc();
+        Assertions.assertTrue(expected.size() > 0);
+
     }
 
     @Test
     void findAllByOrderByIdAsc() {
+
+        Pasaporte pasaporte = Pasaporte.builder()
+                .numero("12345")
+                .pasajero(Pasajero.builder()
+                        .nombre("Jose")
+                        .NID("6789")
+                        .build())
+                .build();
+
+        pasaporteRepository.save(pasaporte);
+
+        List<Pasaporte> expected = pasaporteRepository.findAllByOrderByIdAsc();
+        Assertions.assertTrue(expected.size() > 0);
     }
 
     @Test
     void obtenerPasaportesOrdenadosAsc() {
+
+        Pasaporte pasaporte = Pasaporte.builder()
+                .numero("12345")
+                .pasajero(Pasajero.builder()
+                        .nombre("Jose")
+                        .NID("6789")
+                        .build())
+                .build();
+
+        pasaporteRepository.save(pasaporte);
+
+        List<Pasaporte> expected = pasaporteRepository.findAllByOrderByIdDesc();
+        Assertions.assertTrue(expected.size() > 0);
     }
 
     @Test
     void contarPasaportes() {
+
+        Pasaporte pasaporte = Pasaporte.builder()
+                .numero("12345")
+                .pasajero(Pasajero.builder()
+                        .nombre("Jose")
+                        .NID("6789")
+                        .build())
+                .build();
+
+        pasaporteRepository.save(pasaporte);
+
+        Long count = pasaporteRepository.contarPasaportes();
+        Assertions.assertEquals(1, count);
     }
 
 
@@ -63,15 +160,45 @@ class PasaporteRepositoryTest {
 
         pasaporteRepository.save(pasaporte);
 
-        Pasaporte expected = pasaporteRepository.buscarPorNumero("12345");
+        Pasaporte expected = pasaporteRepository.buscarPorNumero(pasaporte.getNumero());
         Assertions.assertEquals(expected.getNumero(), pasaporte.getNumero());
     }
 
     @Test
     void buscarPorListaIds() {
+
+        Pasaporte pasaporte = Pasaporte.builder()
+                .numero("12345")
+                .pasajero(Pasajero.builder()
+                        .nombre("Jose")
+                        .NID("6789")
+                        .build())
+                .build();
+
+        pasaporteRepository.save(pasaporte);
+        List<Long> ids = new ArrayList<>();
+        ids.add(pasaporte.getId());
+        List<Pasaporte> expected = pasaporteRepository.buscarPorListaIds(ids);
+        Assertions.assertTrue(expected.size() > 0);
+
+
+
     }
 
     @Test
     void buscarPorNumeroParcial() {
+
+        Pasaporte pasaporte = Pasaporte.builder()
+                .numero("12345")
+                .pasajero(Pasajero.builder()
+                        .nombre("Jose")
+                        .NID("6789")
+                        .build())
+                .build();
+
+        pasaporteRepository.save(pasaporte);
+
+
+
     }
 }
